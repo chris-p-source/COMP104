cb77e88d90bcde1e15a99ec9bfe9eb8c38494eaf
jianx <rogerscao0623@gmail.com>
2021-12-22 15:27:34 +0000

    add ethic review form add cw2 instruction add commit_detect.py

---
+++ commit_detect.py
@@ -0,0 +1,10 @@
+from datetime import datetime
+
+from pydriller import repository
+
+dt1 = datetime(2021, 12, 1)
+dt2 = datetime.now()
+
+for commit in repository.Repository("plc4x", since=dt1).traverse_commits():
+    for modified in commit._get_modifications():
+        print(modified.diff)
