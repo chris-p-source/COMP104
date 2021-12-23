d96e3d7eb89bd7fc5d00d32fcb0b158dc743271e
jianx <rogerscao0623@gmail.com>
2021-12-23 18:23:55 +0000

    hbase repo added

--- commit_detect.py
+++ commit_detect.py
@@ -1,10 +1,9 @@
 from datetime import datetime
-
 from pydriller import repository
 
 dt1 = datetime(2021, 12, 1)
 dt2 = datetime.now()
 
-for commit in repository.Repository("plc4x", since=dt1).traverse_commits():
+for commit in repository.Repository("repos/hbase", since=dt1).traverse_commits():
     for modified in commit._get_modifications():
         print(modified.diff)
